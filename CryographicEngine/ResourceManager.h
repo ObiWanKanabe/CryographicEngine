8f3c9515578b5706df02d3c410f9c46ac84584bb
ObiWanKanabe <kanabe.lucas@gmail.com>
2017-09-30 15:37:43 -0400

    Initialized Resource Manager for later work

--- CryographicEngine/ResourceManager.h
+++ CryographicEngine/ResourceManager.h
@@ -1,1 +1,20 @@
-#pragma once
+#ifndef RESOURCEMANAGER_H
+#define RESOURCEMANAGER_H
+
+#include <map>
+#include <vector>
+
+template<class ResourceType>
+
+class ResourceManager {
+public:
+	ResourceManager() {};
+	~ResourceManager() {};
+
+	void addResource() {};
+
+	std::vector<ResourceType*>;
+	std::map<std::string name, Resourcetype type> map;
+};
+#endif
+
